8c7fea7189186f4d2efadec501a938704b63ef5b
Teka Lishoo <teka.lishoo@gmail.com>
2022-05-01 11:35:04 +0300

    Make working comand: poetry run EDA

--- src/forest_cover_type/EDA_pandas_profiling.py
+++ src/forest_cover_type/EDA_pandas_profiling.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-df = pd.read_csv('../../data/train.csv')
-profile = ProfileReport(df, title="Pandas Profiling Report", explorative=True)
-profile.to_file("EDA/pandas_profiling.html")
 
+def eda():
+    df = pd.read_csv('data/train.csv')
+    profile = ProfileReport(df, title="Pandas Profiling Report", explorative=True)
+    profile.to_file("src/forest_cover_type/EDA/pandas_profiling.html")
