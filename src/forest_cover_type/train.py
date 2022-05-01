41693f52fd92bc32803b5d657da59ee3c9a97d98
Teka Lishoo <teka.lishoo@gmail.com>
2022-05-01 11:26:57 +0300

    Added possility to turn on or turn off GridSearch

--- src/forest_cover_type/train.py
+++ src/forest_cover_type/train.py
@@ -28,6 +28,12 @@ from sklearn.model_selection import KFold, cross_val_score, cross_validate
     show_default=True,
 )
 @click.option(
+    "--auto_grid_search",
+    default=True,
+    type=bool,
+    show_default=True,
+)
+@click.option(
     "--scaling",
     default=False,
     type=bool,
@@ -105,11 +111,10 @@ from sklearn.model_selection import KFold, cross_val_score, cross_validate
     type=int,
     show_default=True,
 )
-def train(dataset_path: Path, save_model_path: Path, scaling: bool, select_feature, which_model,
+def train(dataset_path: Path, save_model_path: Path, auto_grid_search: bool, scaling: bool, select_feature, which_model,
           n_estimators, criterion, max_depth,
           penalty, solver, c, fit_intercept, max_iter,
           random_state, cv_k_split):
-
     df = pd.read_csv(dataset_path)
     X = df.drop(columns='Cover_Type')
     y = df['Cover_Type']
@@ -120,41 +125,53 @@ def train(dataset_path: Path, save_model_path: Path, scaling: bool, select_featu
                                 penalty, solver, c, fit_intercept, max_iter, random_state)
 
         cv_inner = KFold(n_splits=cv_k_split, random_state=1, shuffle=True)
-        space = dict()
-        if which_model == 'random_forest':
-            space['mod__n_estimators'] = [50, 100, 200, 300]
-            space['mod__criterion'] = ['gini', 'entropy']
-            space['mod__max_depth'] = [100, 200, 500, 1000]
-        elif which_model == 'log_regr':
-            space['mod__C'] = [0.1, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0]
-            space['mod__fit_intercept'] = [False, True]
 
-        search = GridSearchCV(model, space, scoring='accuracy', n_jobs=1, cv=cv_inner, refit=True)
+        if auto_grid_search:
+            space = dict()
+            if which_model == 'random_forest':
+                space['mod__n_estimators'] = [50, 100, 200, 300]
+                space['mod__criterion'] = ['gini', 'entropy']
+                space['mod__max_depth'] = [100, 200, 500, 1000]
+            elif which_model == 'log_regr':
+                space['mod__C'] = [0.1, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0]
+                space['mod__fit_intercept'] = [False, True]
 
-        cv_outer = KFold(n_splits=10, shuffle=True, random_state=1)
-        scores = cross_validate(search, X, y,
-                                scoring=['accuracy', 'f1_weighted', 'roc_auc_ovr_weighted'], cv=cv_outer,
-                                n_jobs=-1)
-        
+            search = GridSearchCV(model, space, scoring='accuracy', n_jobs=1, cv=cv_inner, refit=True)
 
-        mlflow.log_param("model", which_model)
-        mlflow.log_param("feature_selector", select_feature)
-        if which_model == "random_forest":
-            mlflow.log_param("use_scaler", scaling)
-            mlflow.log_param("n_estimators", n_estimators)
-            mlflow.log_param("criterion", criterion)
-            mlflow.log_param("max_depth", max_depth)
-        elif which_model == "log_regr":
-            mlflow.log_param("use_scaler", True)
-            mlflow.log_param("penalty", penalty)
-            mlflow.log_param("c", c)
-            mlflow.log_param("fit_intercept", fit_intercept)
-            mlflow.log_param("max_iter", max_iter)
+            cv_outer = KFold(n_splits=10, shuffle=True, random_state=1)
+            scores = cross_validate(search, X, y,
+                                    scoring=['accuracy', 'f1_weighted', 'roc_auc_ovr_weighted'], cv=cv_outer,
+                                    n_jobs=-1)
+            result = search.fit(X, y)
+            best_model = result.best_estimator_
+            click.echo('Best parameters')
+            click.echo(search.best_params_)
 
-        mlflow.log_metric("accuracy", np.mean(scores["test_accuracy"]))
-        mlflow.log_metric("F1", np.mean(scores["test_f1_weighted"]))
-        mlflow.log_metric("ROC AUC", np.mean(scores["test_roc_auc_ovr_weighted"]))
-        mlflow.sklearn.log_model(model, which_model)
+        else:
+            scores = cross_validate(model, X, y,
+                                    scoring=['accuracy', 'f1_weighted', 'roc_auc_ovr_weighted'], cv=cv_inner,
+                                    n_jobs=-1)
+            mlflow.log_param("model", which_model)
+            mlflow.log_param("feature_selector", select_feature)
+            if which_model == "random_forest":
+                mlflow.log_param("use_scaler", scaling)
+                mlflow.log_param("n_estimators", n_estimators)
+                mlflow.log_param("criterion", criterion)
+                mlflow.log_param("max_depth", max_depth)
+            elif which_model == "log_regr":
+                mlflow.log_param("use_scaler", True)
+                mlflow.log_param("penalty", penalty)
+                mlflow.log_param("c", c)
+                mlflow.log_param("fit_intercept", fit_intercept)
+                mlflow.log_param("max_iter", max_iter)
+
+            mlflow.log_metric("accuracy", np.mean(scores["test_accuracy"]))
+            mlflow.log_metric("F1", np.mean(scores["test_f1_weighted"]))
+            mlflow.log_metric("ROC AUC", np.mean(scores["test_roc_auc_ovr_weighted"]))
+            mlflow.sklearn.log_model(model, which_model)
+
+            best_model = model
+            best_model.fit(X, y)
 
         click.echo(
             'Accuracy mean: %.3f, with std: %.3f' % (np.mean(scores["test_accuracy"]), np.std(scores["test_accuracy"])))
@@ -163,10 +180,6 @@ def train(dataset_path: Path, save_model_path: Path, scaling: bool, select_featu
         click.echo('ROC AUC mean: %.3f, with std: %.3f' % (
             np.mean(scores["test_roc_auc_ovr_weighted"]), np.std(scores["test_roc_auc_ovr_weighted"])))
 
-        result = search.fit(X, y)
-        best_model = result.best_estimator_
-        click.echo('Best parameters')
-        click.echo(search.best_params_)
         y_pred = best_model.predict(X)
 
         dump(best_model, save_model_path)
